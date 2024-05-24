0dcbf9336ff822f54431216cd45c8701950090c5
smashyalts <smashyalts@gmail.com>
2024-05-24 14:53:54 +0200

    PUSH

--- src/main/java/net/craftsupport/calculator/HelloApplication.java
+++ src/main/java/net/craftsupport/calculator/HelloApplication.java
@@ -1,21 +1,41 @@
 package net.craftsupport.calculator;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    static Stage backup;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 480, 420);
+        stage.setTitle("Pythagores Læresætning");
         stage.setScene(scene);
         stage.show();
+        backup = stage;
     }
+
+    public static Stage getBackup() {
+        return backup;
+    }
+
+    public class PageNavigation {
+        public static void showPage(Class<?> appClass, String fxmlPath, Stage stage, double width, double height) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(appClass.getResource(fxmlPath));
+            Scene scene = new Scene(fxmlLoader.load(), width, height);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/net/craftsupport/calculator/HelloController.java
+++ src/main/java/net/craftsupport/calculator/HelloController.java
@@ -1,14 +1,57 @@
 package net.craftsupport.calculator;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField hypotenuse;
+    @FXML
+    private TextField adjacent;
+    @FXML
+    private TextField opposite;
+    @FXML
+    private Label Result;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onCalculateButtonClick() {
+        if (hypotenuse.getText().isEmpty() && !adjacent.getText().isEmpty() && !opposite.getText().isEmpty()) {
+            calculateHypotenuse();
+        } else if (!hypotenuse.getText().isEmpty() && adjacent.getText().isEmpty() && !opposite.getText().isEmpty()) {
+            calculateAdjacent();
+        } else if (!hypotenuse.getText().isEmpty() && !adjacent.getText().isEmpty() && opposite.getText().isEmpty()) {
+            calculateOpposite();
+        }
+    }
+
+@FXML
+private void onPageButtonClick2() throws IOException {
+Stage stage = HelloApplication.getBackup();
+    HelloApplication.PageNavigation.showPage(HelloApplication.class, "hello-view2.fxml", stage, 480, 420);
+}
+    private void calculateOpposite() {
+        double a = Double.parseDouble(adjacent.getText());
+        double h = Double.parseDouble(hypotenuse.getText());
+        double o = Math.sqrt(h * h - a * a);
+        Result.setText("Opposite side is: " + o);
+    }
+    private void calculateAdjacent() {
+        double o = Double.parseDouble(opposite.getText());
+        double h = Double.parseDouble(hypotenuse.getText());
+        double a = Math.sqrt(h * h - o * o);
+        Result.setText("Adjacent side is: " + a);
+    }
+    private void calculateHypotenuse() {
+        double a = Double.parseDouble(adjacent.getText());
+        double o = Double.parseDouble(opposite.getText());
+        double h = Math.sqrt(a * a + o * o);
+        Result.setText("Hypotenuse is: " + h);
     }
 }
